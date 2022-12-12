0603cc81488054a50597a22011f0b558eae40eac
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:38:51 -0600

    function productFib

--- index.js
+++ index.js
@@ -87,3 +87,12 @@ function dirReduc(arr) {
   );
 }
 //Product of consecutive Fib numbers
+function productFib(prod) {
+  var n = 0;
+  var nPlus = 1;
+  while (n * nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n * nPlus === prod];
+}
