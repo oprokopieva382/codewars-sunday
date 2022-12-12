7e910d7006a98cc3a3b47b37f13106eca58354ff
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:37:24 -0600

    function dirReduc

--- index.js
+++ index.js
@@ -72,3 +72,17 @@ function dividedBy(b) {
   };
 }
 //Directions Reduction
+function dirReduc(arr) {
+  const opposites = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return arr.reduce(
+    (acc, cur) => (
+      opposites[acc.slice(-1)] === cur ? acc.pop() : acc.push(cur), acc
+    ),
+    []
+  );
+}
