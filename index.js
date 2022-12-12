76361b5a9cd82f9bbc03f6275a5ceca89d471951
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:30:21 -0600

    mostMoney

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ String.prototype.isUpperCase = function () {
   return this.toString() === this.toUpperCase();
 };
 //Who has the most money?
+function mostMoney(students) {
+  let array = [];
+  if (students.length === 1) {
+    return students[0].name;
+  }
+  students.forEach((value, index) => {
+    let total = 5 * value.fives + 10 * value.tens + 20 * value.twenties;
+    array.push([total, value.name]);
+  });
+  array = array.sort((a, b) => b[0] - a[0]);
+  if (array.every((el, i, array) => el[0] === array[0][0])) {
+    return "all";
+  } else {
+    return array[0][1];
+  }
+}
